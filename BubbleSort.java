6b6630b229ac1ea81e7a92cafd12d129a3f1ae80
Your Name <you@example.com>
2018-08-11 20:48:44 +0800

    sss

--- BubbleSort.java
+++ BubbleSort.java
@@ -12,6 +12,7 @@ public class BubbleSort {
         for (int i = 0; i < length -1; i++) {
             for (int j = 0; j < length - 1 - i; j++) {
                 if (arr[j] < arr[j+1]) {
+                    //异或方式交换两个值
                     arr[j] = arr[j] ^ arr[j+1];
                     arr[j+1] = arr[j] ^ arr[j+1];
                     arr[j] = arr[j] ^ arr[j+1];
